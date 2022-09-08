5f85fb73ea32beea57537b868aada53c90d9bff9
Davi Feliciano <dfeliciano37@gmail.com>
2022-09-07 23:17:02 -0300

    Incluindo gráficos para os níveis de Landau para o vale K', renderizando gráficos de gap em termos de B, pequenas correções

--- scripts/landau_levels.py
+++ scripts/landau_levels.py
@@ -1,6 +1,5 @@
 from fileinput import filename
 from pathlib import Path
-from turtle import title
 from typing import Tuple
 import numpy as np
 from numpy.typing import NDArray
@@ -58,8 +57,10 @@ def landau_levels(
 
     Returns
     -------
-    Tuple[NDArray, NDArray]
-        the sorted eigenvalues of for the first order k.p hamiltonian
+    Tuple[NDArray | None, NDArray | None]
+        A tuple of NDArrays being the valence and conduction bands of for the
+        first order k.p hamiltonian. If n = 0, the correspondent level may not
+        exist, and the respective band in the tuple will be replaced with None
 
     Raises
     ------
@@ -77,6 +78,14 @@ def landau_levels(
         spin = -1
 
     n = abs(int(n))
+    if n == 0:
+        if valley_index == 1:
+            result = np.full_like(mag_field, fill_value=-0.5 * delta + lambda_so * spin)
+            return result, None
+        if valley_index == -1:
+            result = np.full_like(mag_field, fill_value=0.5 * delta)
+            return None, result
+
     mag_lenght = np.sqrt(REDUCED_PLANCK / (ELEM_CHARGE * mag_field))
     lambda_valley_spin = lambda_so * valley_index * spin
     omega = np.sqrt(2) / mag_lenght
@@ -92,63 +101,113 @@ def landau_levels(
 project_dir = Path(__file__).parent.parent
 images_dir = project_dir.joinpath("imagens")
 results_dir = project_dir.joinpath("results")
+
+valleys = {"k_valley": 1, "k_prime_valley": -1}
 lattices = {"crs2": CRS2_LATTICE, "crse2": CRSE2_LATTICE}
 titles = {"crs2": r"\ch{CrS2}", "crse2": r"\ch{CrSe2}"}
-mag_field = np.linspace(0, 10, 100)
-
 
 for lattice_name, lattice_const in lattices.items():
+    for valley_name, valley_index in valleys.items():
 
-    # Reading files
-    file = results_dir.joinpath(f"{lattice_name}_genetic_algorithm_order_13.csv")
-    df = pd.read_csv(file, index_col=0)
-    delta, lambda_c, lambda_v, gamma_0 = df.loc["delta":"gamma_0", "order_1"]
+        # Reading files
+        file = results_dir.joinpath(f"{lattice_name}_genetic_algorithm_order_13.csv")
+        df = pd.read_csv(file, index_col=0)
+        delta, lambda_c, lambda_v, gamma_0 = df.loc["delta":"gamma_0", "order_1"]
 
-    # Creating figures
-    fig, axes = plt.subplots(1, 2)
-    ax_titles = ("Valência", "Condução")
-    lambdas = (lambda_v, lambda_c)
-    lambda_labels = (r"\Psi_V", r"\Psi_C")
+        # Creating landau levels plot
+        fig, axes = plt.subplots(1, 2)
+        ax_titles = ("Valência", "Condução")
+        lambdas = (lambda_v, lambda_c)
+        lambda_labels = (r"\Psi_V", r"\Psi_C")
 
-    for i, (ax, ax_title, lambda_label) in enumerate(
-        zip(axes, ax_titles, lambda_labels)
-    ):
-        ax.set(
-            title=ax_title,
-            ylabel=r"Energia (\si{\eV})" if i == 0 else None,
-            xlabel=r"$B$ (\si{\tesla})",
-        )
+        for i, (ax, ax_title, lambda_label) in enumerate(
+            zip(axes, ax_titles, lambda_labels)
+        ):
+            ax.set(
+                title=ax_title,
+                ylabel=r"Energia (\si{\eV})" if i == 0 else None,
+                xlabel=r"$B$ (\si{\tesla})",
+            )
 
-        spins = ("down", "up")
-        spin_labels = (r"\downarrow", r"\uparrow")
-        colors = ("blue", "red")
+            spins = ("down", "up")
+            spin_labels = (r"\downarrow", r"\uparrow")
+            colors = ("blue", "red")
+            mag_field = np.linspace(0, 10, 100)
 
-        for spin, color, spin_label in zip(spins, colors, spin_labels):
-            for n in range(10):
-                eigen = landau_levels(
-                    lattice_const=lattice_const,
-                    delta=delta,
-                    lambda_so=lambda_v,
-                    gamma_0=gamma_0,
-                    valley_index=1,
-                    mag_field=mag_field,
-                    spin=spin,
-                    n=n,
-                )[i]
+            for spin, color, spin_label in zip(spins, colors, spin_labels):
+                for n in range(10):
+                    eigen = landau_levels(
+                        lattice_const=lattice_const,
+                        delta=delta,
+                        lambda_so=lambda_v,
+                        gamma_0=gamma_0,
+                        valley_index=valley_index,
+                        mag_field=mag_field,
+                        spin=spin,
+                        n=n,
+                    )[i]
 
-                ax.plot(
-                    mag_field,
-                    eigen,
-                    color=color,
-                    linewidth=1.0,
-                    label=r"$\expval{\hat{H}}{" + f"{lambda_label},{spin_label}" + "}$",
-                )
+                    try:
+                        ax.plot(
+                            mag_field,
+                            eigen,
+                            color=color,
+                            linewidth=1.0,
+                            label=r"$\expval{\hat{H}}{"
+                            + f"{lambda_label},{spin_label}"
+                            + "}$",
+                        )
+                    except ValueError:
+                        pass
 
-        # Removing repeated entries from legend
-        handles, eigen_labels = ax.get_legend_handles_labels()
-        by_label = dict(zip(eigen_labels, handles))
-        ax.legend(by_label.values(), by_label.keys(), framealpha=1.0)
+            # Removing repeated entries from legend
+            handles, eigen_labels = ax.get_legend_handles_labels()
+            by_label = dict(zip(eigen_labels, handles))
+            ax.legend(by_label.values(), by_label.keys(), framealpha=1.0)
 
-    basename = f"{lattice_name}_landau_levels"
+        basename = f"{lattice_name}_{valley_name}_landau_levels"
+        filename = images_dir.joinpath(f"{basename}.png")
+        plt.savefig(filename, dpi=300)
+
+    # Creating gap vs mag_field plot
+    fig, ax = plt.subplots()
+    ax.set(
+        title=titles[lattice_name],
+        ylabel=r"$\Delta$ (\si{\eV})",
+        xlabel=r"$B$ (\si{\tesla})",
+    )
+
+    colors = ("blue", "red")
+    mag_field = np.linspace(1, 1e7, 1000)
+
+    for color, (valley_name, valley_index) in zip(colors, valleys.items()):
+        cond_down_level = landau_levels(
+            lattice_const=lattice_const,
+            delta=delta,
+            lambda_so=lambda_v,
+            gamma_0=gamma_0,
+            valley_index=valley_index,
+            mag_field=mag_field,
+            spin="down",
+            n=1 if valley_index == 1 else 0,
+        )[1]
+
+        valence_up_level = landau_levels(
+            lattice_const=lattice_const,
+            delta=delta,
+            lambda_so=lambda_v,
+            gamma_0=gamma_0,
+            valley_index=valley_index,
+            mag_field=mag_field,
+            spin="up",
+            n=0 if valley_index == 1 else 1,
+        )[0]
+
+        bandgap = cond_down_level - valence_up_level
+        ax.plot(mag_field, bandgap, color=color, label=rf"$\tau = {valley_index}$")
+
+    ax.legend()
+
+    basename = f"{lattice_name}_bandgap_field"
     filename = images_dir.joinpath(f"{basename}.png")
     plt.savefig(filename, dpi=300)
